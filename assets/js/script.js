9cdc265401411e7d97cf7a7b7b953f58e4e54379
Lowell Hallassou <lo@Lowells-MacBook-Air.local>
2024-01-22 16:20:25 +0000

    Created array for business hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+let businessHours = [
+    dayjs().set('hour',9).set('minute', 0).set('second', 0),
+    dayjs().set('hour',10).set('minute', 0).set('second', 0),
+    dayjs().set('hour',11).set('minute', 0).set('second', 0),
+    dayjs().set('hour',12).set('minute', 0).set('second', 0),
+    dayjs().set('hour',13).set('minute', 0).set('second', 0),
+    dayjs().set('hour',14).set('minute', 0).set('second', 0),
+    dayjs().set('hour',15).set('minute', 0).set('second', 0),
+    dayjs().set('hour',16).set('minute', 0).set('second', 0),
+    dayjs().set('hour',17).set('minute', 0).set('second', 0),
+]
+
+
 $('#currentDay').text(dayjs().format('dddgd, MMMM D'));
+
+
